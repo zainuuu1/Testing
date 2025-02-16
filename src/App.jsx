54f20213f009068ed65bdbf22e572e5a33f238c2
zainuuu1 <197623286+zainuuu1@users.noreply.github.com~>
2025-02-16 15:47:05 +0500

    my all files without ignore files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>HELLO FUTURE DEVOPS ENGINEER!</h1>
+      <h2>Congrats for your first DEPLOYMENT!</h2>
+    </>
+  )
+}
+
+export default App
